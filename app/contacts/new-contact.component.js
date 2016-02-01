d3787fcf038dd385892e838d11c8fdbb62578233
John Lundgren <john.paul.lundgren@gmail.com>
2016-02-01 13:09:31 +0100

    Added formbuilder functionality

--- app/contacts/new-contact.component.js
+++ app/contacts/new-contact.component.js
@@ -1,4 +1,4 @@
-System.register(["angular2/core", "./contact.service", "angular2/router"], function(exports_1) {
+System.register(["angular2/core", "./contact.service", "angular2/router", "angular2/common"], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,7 +8,7 @@ System.register(["angular2/core", "./contact.service", "angular2/router"], funct
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, contact_service_1, router_1;
+    var core_1, contact_service_1, router_1, common_1;
     var NewContactComponent;
     return {
         setters:[
@@ -20,34 +20,38 @@ System.register(["angular2/core", "./contact.service", "angular2/router"], funct
             },
             function (router_1_1) {
                 router_1 = router_1_1;
+            },
+            function (common_1_1) {
+                common_1 = common_1_1;
             }],
         execute: function() {
             NewContactComponent = (function () {
                 //The Router does not need to be added to the providers list because it is generated in the boot.ts file
-                function NewContactComponent(_contactService, _router, _routeParams) {
+                function NewContactComponent(_contactService, _router, _routeParams, _formBuilder) {
                     this._contactService = _contactService;
                     this._router = _router;
                     this._routeParams = _routeParams;
+                    this._formBuilder = _formBuilder;
                 }
-                NewContactComponent.prototype.onSubmit = function () {
-                    this._contactService.insertContact(this.newContact);
+                NewContactComponent.prototype.onSubmit = function (formValue) {
+                    this._contactService.insertContact(formValue);
                     this._router.navigate(['Contacts']);
                 };
                 NewContactComponent.prototype.ngOnInit = function () {
-                    this.newContact = {
-                        firstName: '',
-                        lastName: this._routeParams.get('lastName'),
-                        email: '',
-                        phone: ''
-                    };
+                    this.myForm = this._formBuilder.group({
+                        firstName: ['', common_1.Validators.required],
+                        lastName: [this._routeParams.get('lastName'), common_1.Validators.required],
+                        email: ['', common_1.Validators.required],
+                        phone: ['', common_1.Validators.required]
+                    });
                 };
                 NewContactComponent = __decorate([
                     core_1.Component({
-                        template: "\n     <form #myForm=\"ngForm\" (ngSubmit)=\"onSubmit()\">\n        <div>\n            <label>First name:</label>\n            <input type=\"text\" required ngControl=\"firstName\" [(ngModel)]=\"newContact.firstName\" #firstName=\"ngForm\">\n            <span *ngIf=\"!firstName.valid\">Not valid</span>\n        </div>\n        <div>\n            <label>Last name:</label>\n            <input type=\"text\" required ngControl=\"lastName\" [(ngModel)]=\"newContact.lastName\" #lastName=\"ngForm\">\n            <span *ngIf=\"!lastName.valid\">Not valid</span>\n        </div>\n        <div>\n            <label>Phone:</label>\n            <input type=\"text\" required ngControl=\"phone\" [(ngModel)]=\"newContact.phone\" #phone=\"ngForm\">\n            <span *ngIf=\"!phone.valid\">Not valid</span>\n        </div>\n        <div>\n            <label>Email:</label>\n            <input type=\"text\" required ngControl=\"email\" [(ngModel)]=\"newContact.email\" #email=\"ngForm\">\n            <span *ngIf=\"!email.valid\">Not valid</span>\n        </div>\n        <button type=\"submit\" [disabled]=\"!myForm.form.valid\">Create Contact</button>\n    </form>\n    ",
+                        template: "\n     <form [ngFormModel]=\"myForm\" (ngSubmit)=\"onSubmit(myForm.value)\">\n        <div>\n            <label>First name:</label>\n            <input type=\"text\" [ngFormControl]=\"myForm.controls['firstName']\" #firstName=\"ngForm\">\n            <span *ngIf=\"!firstName.valid\">Not valid</span>\n        </div>\n        <div>\n            <label>Last name:</label>\n            <input type=\"text\" [ngFormControl]=\"myForm.controls['lastName']\" #lastName=\"ngForm\">\n            <span *ngIf=\"!lastName.valid\">Not valid</span>\n        </div>\n        <div>\n            <label>Phone:</label>\n            <input type=\"text\" [ngFormControl]=\"myForm.controls['phone']\" #phone=\"ngForm\">\n            <span *ngIf=\"!phone.valid\">Not valid</span>\n        </div>\n        <div>\n            <label>Email:</label>\n            <input type=\"text\" [ngFormControl]=\"myForm.controls['email']\" #email=\"ngForm\">\n            <span *ngIf=\"!email.valid\">Not valid</span>\n        </div>\n        <button type=\"submit\" [disabled]=\"!myForm.valid\">Create Contact</button>\n    </form>\n    ",
                         styles: ["\n        .ng-invalid{\n            border:1px solid red;\n        }\n        form div span{\n            color: red;\n        }\n    "],
                         providers: [contact_service_1.ContactService]
                     }), 
-                    __metadata('design:paramtypes', [contact_service_1.ContactService, router_1.Router, router_1.RouteParams])
+                    __metadata('design:paramtypes', [contact_service_1.ContactService, router_1.Router, router_1.RouteParams, common_1.FormBuilder])
                 ], NewContactComponent);
                 return NewContactComponent;
             })();
--- app/contacts/new-contact.component.ts
+++ app/contacts/new-contact.component.ts
@@ -2,31 +2,32 @@ import {Component, OnInit} from "angular2/core";
 import {ContactService} from "./contact.service";
 import {Contact} from "./contact";
 import {Router, RouteParams} from "angular2/router";
+import {ControlGroup, FormBuilder, Validators} from "angular2/common";
 
 @Component({
     template:`
-     <form #myForm="ngForm" (ngSubmit)="onSubmit()">
+     <form [ngFormModel]="myForm" (ngSubmit)="onSubmit(myForm.value)">
         <div>
             <label>First name:</label>
-            <input type="text" required ngControl="firstName" [(ngModel)]="newContact.firstName" #firstName="ngForm">
+            <input type="text" [ngFormControl]="myForm.controls['firstName']" #firstName="ngForm">
             <span *ngIf="!firstName.valid">Not valid</span>
         </div>
         <div>
             <label>Last name:</label>
-            <input type="text" required ngControl="lastName" [(ngModel)]="newContact.lastName" #lastName="ngForm">
+            <input type="text" [ngFormControl]="myForm.controls['lastName']" #lastName="ngForm">
             <span *ngIf="!lastName.valid">Not valid</span>
         </div>
         <div>
             <label>Phone:</label>
-            <input type="text" required ngControl="phone" [(ngModel)]="newContact.phone" #phone="ngForm">
+            <input type="text" [ngFormControl]="myForm.controls['phone']" #phone="ngForm">
             <span *ngIf="!phone.valid">Not valid</span>
         </div>
         <div>
             <label>Email:</label>
-            <input type="text" required ngControl="email" [(ngModel)]="newContact.email" #email="ngForm">
+            <input type="text" [ngFormControl]="myForm.controls['email']" #email="ngForm">
             <span *ngIf="!email.valid">Not valid</span>
         </div>
-        <button type="submit" [disabled]="!myForm.form.valid">Create Contact</button>
+        <button type="submit" [disabled]="!myForm.valid">Create Contact</button>
     </form>
     `,
     styles: [`
@@ -42,23 +43,23 @@ import {Router, RouteParams} from "angular2/router";
 
 export class NewContactComponent implements OnInit {
     
-    public newContact: Contact;
+    public myForm: ControlGroup;
     
     //The Router does not need to be added to the providers list because it is generated in the boot.ts file
-    constructor(private _contactService: ContactService, private _router: Router, private _routeParams: RouteParams) {}
+    constructor(private _contactService: ContactService, private _router: Router, private _routeParams: RouteParams, private _formBuilder: FormBuilder) {}
     
-    onSubmit(){
-        this._contactService.insertContact(this.newContact);
+    onSubmit(formValue){
+        this._contactService.insertContact(formValue);
         this._router.navigate(['Contacts']);
     }
     
     ngOnInit():any {
-        this.newContact = {
-            firstName: '',
-            lastName: this._routeParams.get('lastName'),
-            email: '',
-            phone: ''
-        };
+        this.myForm = this._formBuilder.group({
+            firstName: ['', Validators.required],
+            lastName: [this._routeParams.get('lastName'), Validators.required],
+            email: ['', Validators.required],
+            phone: ['', Validators.required]
+        });
     }
     
 }
